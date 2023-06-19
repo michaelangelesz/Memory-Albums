8d4883ad97ae1ff680ddc739a81252001e1eb1de
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-19 03:06:16 -0400

    generate cards from javascript

--- main.js
+++ main.js
@@ -1,10 +1,69 @@
 const cards = document.querySelectorAll(".memory-card");
+const section = document.querySelector("section");
+section.setAttribute("class", "memory-game");
 
 const playerLivesCount = document.querySelector("span");
 let playerLives = 16;
 
 // link text
 playerLivesCount.textContent = playerLives;
+
+// generate the data
+const frameworks = [
+  { imgSrc: "../images/bboys.jpeg", name: "bboys", alt: "Paul's Boutique" },
+  { imgSrc: "../images/bboys.jpeg", name: "bboys", alt: "Paul's Boutique" },
+  { imgSrc: "../images/bjork.jpeg", name: "bjork", alt: "Debut" },
+  { imgSrc: "../images/bjork.jpeg", name: "bjork", alt: "Debut" },
+  { imgSrc: "../images/cure.jpeg", name: "cure", alt: "Three Imaginary Boys" },
+  { imgSrc: "../images/cure.jpeg", name: "cure", alt: "Three Imaginary Boys" },
+  { imgSrc: "../images/doors.jpeg", name: "doors", alt: "Morrison Hotel" },
+  { imgSrc: "../images/doors.jpeg", name: "doors", alt: "Morrison Hotel" },
+  { imgSrc: "../images/floyd.jpeg", name: "floyd", alt: "Dark Side Of The Moon" },
+  { imgSrc: "../images/floyd.jpeg", name: "floyd", alt: "Dark Side Of The Moon" },
+  { imgSrc: "../images/jimi.jpeg", name: "jimi", alt: "Electric Ladyland" },
+  { imgSrc: "../images/jimi.jpeg", name: "jimi", alt: "Electric Ladyland" },
+  { imgSrc: "../images/lusciousj.jpeg", name: "lusciousj", alt: "Fever In Fever Out" },
+  { imgSrc: "../images/lusciousj.jpeg", name: "lusciousj", alt: "Fever In Fever Out" },
+  { imgSrc: "../images/madonna.jpeg", name: "madonna", alt: "Like A Virgin" },
+  { imgSrc: "../images/madonna.jpeg", name: "madonna", alt: "Like A Virgin" },
+  { imgSrc: "../images/marley.jpeg", name: "marley", alt: "Legend" },
+  { imgSrc: "../images/marley.jpeg", name: "marley", alt: "Legend" },
+  { imgSrc: "../images/redhot.jpeg", name: "redhot", alt: "One Hot Minute" },
+  { imgSrc: "../images/redhot.jpeg", name: "redhot", alt: "One Hot Minute" },
+  { imgSrc: "../images/tribe.jpeg", name: "tribe", alt: "The Low End Theory" },
+  { imgSrc: "../images/tribe.jpeg", name: "tribe", alt: "The Low End Theory" },
+  { imgSrc: "../images/u2.jpeg", name: "u2", alt: "The Joshua Tree" },
+  { imgSrc: "../images/u2.jpeg", name: "u2", alt: "The Joshua Tree" },
+]; 
+
+// loop through the frameworks array and create a new card element for each one
+for (let i = 0; i < frameworks.length; i++) {
+  // create a new div element
+  const cardDiv = document.createElement("div");
+
+  // set the class and data-framework attributes of the div element
+  cardDiv.setAttribute("class", "memory-card");
+  cardDiv.setAttribute("data-framework", frameworks[i].name);
+
+  // create the front-face image element
+  const frontImage = document.createElement("img");
+  frontImage.setAttribute("class", "front-face");
+  frontImage.setAttribute("src", frameworks[i].imgSrc);
+  frontImage.setAttribute("alt", frameworks[i].alt);
+
+  // create the back-face image element
+  const backImage = document.createElement("img");
+  backImage.setAttribute("class", "back-face");
+  backImage.setAttribute("src", "images/cassette.jpeg");
+  backImage.setAttribute("alt", "Back of Card");
+
+  // append the image elements to the card div element
+  cardDiv.appendChild(frontImage);
+  cardDiv.appendChild(backImage);
+
+  // append the card div element to the parent element
+  section.appendChild(cardDiv);
+}
 
 let intialFlip = false;
 let hasFlippedCard = false;
@@ -76,12 +135,12 @@ function checkForMatch() {
   }
 }
 
-(function shuffle() {
+(function shuffle(cards) {
   cards.forEach((card) => {
     let randomPos = Math.floor(Math.random() * 18);
     card.style.order = randomPos;
   });
-})();
+})(document.querySelectorAll(".memory-card"));
 
 // show message function
 function showMessage(text, buttonText) {
