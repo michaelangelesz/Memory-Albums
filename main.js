861be0a97bed34212a6d30c04d8f7cb3a3850c89
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-19 19:53:30 -0400

    extravagant javascript

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 // const cards = document.querySelectorAll(".memory-card");
 const section = document.querySelector("section");
-section.setAttribute("class", "memory-game");
+section.classList.add("memory-game");
 
 const playerLivesCount = document.querySelector("span");
 let playerLives = 16;
-
 // link text
 playerLivesCount.textContent = playerLives;
 
@@ -18,12 +17,28 @@ const getData = () => [
   { imgSrc: "../images/cure.jpeg", name: "cure", alt: "Three Imaginary Boys" },
   { imgSrc: "../images/doors.jpeg", name: "doors", alt: "Morrison Hotel" },
   { imgSrc: "../images/doors.jpeg", name: "doors", alt: "Morrison Hotel" },
-  { imgSrc: "../images/floyd.jpeg", name: "floyd", alt: "Dark Side Of The Moon" },
-  { imgSrc: "../images/floyd.jpeg", name: "floyd", alt: "Dark Side Of The Moon" },
+  {
+    imgSrc: "../images/floyd.jpeg",
+    name: "floyd",
+    alt: "Dark Side Of The Moon",
+  },
+  {
+    imgSrc: "../images/floyd.jpeg",
+    name: "floyd",
+    alt: "Dark Side Of The Moon",
+  },
   { imgSrc: "../images/jimi.jpeg", name: "jimi", alt: "Electric Ladyland" },
   { imgSrc: "../images/jimi.jpeg", name: "jimi", alt: "Electric Ladyland" },
-  { imgSrc: "../images/lusciousj.jpeg", name: "lusciousj", alt: "Fever In Fever Out" },
-  { imgSrc: "../images/lusciousj.jpeg", name: "lusciousj", alt: "Fever In Fever Out" },
+  {
+    imgSrc: "../images/lusciousj.jpeg",
+    name: "lusciousj",
+    alt: "Fever In Fever Out",
+  },
+  {
+    imgSrc: "../images/lusciousj.jpeg",
+    name: "lusciousj",
+    alt: "Fever In Fever Out",
+  },
   { imgSrc: "../images/madonna.jpeg", name: "madonna", alt: "Like A Virgin" },
   { imgSrc: "../images/madonna.jpeg", name: "madonna", alt: "Like A Virgin" },
   { imgSrc: "../images/marley.jpeg", name: "marley", alt: "Legend" },
@@ -34,159 +49,130 @@ const getData = () => [
   { imgSrc: "../images/tribe.jpeg", name: "tribe", alt: "The Low End Theory" },
   { imgSrc: "../images/u2.jpeg", name: "u2", alt: "The Joshua Tree" },
   { imgSrc: "../images/u2.jpeg", name: "u2", alt: "The Joshua Tree" },
-]; 
+];
 
-// shuffle the cards
+// randomize the cards
 const randomize = () => {
   const cardData = getData();
   cardData.sort(() => Math.random() - 0.5); // randomize the cards
   return cardData;
 };
-
 // card generator
 const cardGenerator = () => {
-  // call shuffle when cards are generated
   const cardData = randomize();
-  // // generate the html
+  // generate the html
+  const cards = document.querySelectorAll(".card");
   cardData.forEach((item) => {
-    console.log(item);
     const card = document.createElement("div");
     const face = document.createElement("img");
     const back = document.createElement("img");
-    // add the info to the cards
     card.classList = "card";
     face.classList = "face";
     back.classList = "back";
     // attach the info to the cards
     face.src = item.imgSrc;
     back.src = "../images/cassette.jpeg";
-    card.dataset.framework = item.alt; // add data-framework attribute
+    card.setAttribute("name", item.name);
     // attach the info to the section
     section.appendChild(card);
     card.appendChild(face);
     card.appendChild(back);
+    // make the cards flip
+    card.addEventListener("click", (e) => {
+      face.classList.toggle("toggleCard");
+      card.classList.toggle("toggleCard");
+      checkCards(e);
+    });
   });
-  console.log(cardData);
 };
 
-// let intialFlip = false;
-// let hasFlippedCard = false;
-// let lockBoard = false;
-// let firstCard, secondCard;
+let count = 0;
 
-// function flipCard() {
-//   if (!intialFlip) {
-//     intialFlip = true;
-//   }
+// check for a match
+function checkCards(e) {
+  console.log(e);
+  const clickedCard = e.target;
+  clickedCard.classList.add("flipped");
+  const flippedCards = document.querySelectorAll(".flipped");
+  const toggleCard = document.querySelectorAll(".toggleCard");
+  console.log(flippedCards);
+  // logic:
+  if (flippedCards.length === 2) {
+    if (
+      flippedCards[0].getAttribute("name") ===
+      flippedCards[1].getAttribute("name")
+    ) {
+      console.log("match");
+      // leave cards flipped if match
+      flippedCards.forEach((card) => {
+        card.classList.remove("flipped");
+        card.style.pointerEvents = "none";
+        // count each time a match is made, needed for win function
+        count++;
+        console.log("Function has been called " + count + " times.");
+        if (count === 24) {
+          restart("Awesome! 🤘 You rock!");
+        }
+      });
+    } else {
+      console.log("wrong");
+      // flip cards back if not match
+      flippedCards.forEach((card) => {
+        card.classList.remove("flipped");
+        setTimeout(() => {
+          card.classList.remove("toggleCard");
+        }, 1000);
+      });
+      playerLives--;
+      playerLivesCount.textContent = playerLives;
+      if (playerLives === 0)
+        setTimeout(() => {
+          restart("Bummer! 😵‍💫 You Lose!");
+        }, 500);
+    }
+  }
+}
 
-//   // locks board so you can't click more than 2 cards at a time
-//   if (lockBoard) return;
-//   // prevents double clicking on same card
-//   if (this === firstCard) return;
-//   this.classList.add("flip");
-
-//   if (!hasFlippedCard) {
-//     // first click
-//     hasFlippedCard = true;
-//     firstCard = this;
-//     return;
-//   }
-
-//   // second click
-//   secondCard = this;
-//   checkForMatch();
-// }
-
-// let count = 0;
-
-// function checkForMatch() {
-//   // ternary operator is if/else statement in one line
-//   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-//   // condition ? true : false
-//   isMatch ? disableCards() : unflipCards();
-
-//   function disableCards() {
-//     firstCard.removeEventListener("click", flipCard);
-//     secondCard.removeEventListener("click", flipCard);
-//     count++;
-//     console.log("Function has been called " + count + " times.");
-//     if (count === 12) {
-//       winGame();
-//     }
-//     resetBoard();
-//   }
-
-//   function unflipCards() {
-//     // lock the board
-//     lockBoard = true;
-//     setTimeout(() => {
-//       firstCard.classList.remove("flip");
-//       secondCard.classList.remove("flip");
-//       // unlocks board after 1.5 seconds
-//       resetBoard();
-//     }, 1500);
-//     playerLives--;
-//     playerLivesCount.textContent = playerLives;
-//     if (playerLives === 0)
-//       setTimeout(() => {
-//         gameOver();
-//       }, 500);
-//   }
-
-//   function resetBoard() {
-//     [hasFlippedCard, lockBoard] = [false, false];
-//     [firstCard, secondCard] = [null, null];
-//   }
-// }
-
-// // (function shuffle(cards) {
-// //   cards.forEach((card) => {
-// //     let randomPos = Math.floor(Math.random() * 18);
-// //     card.style.order = randomPos;
-// //   });
-// // })(document.querySelectorAll(".memory-card"));
-
-// // show message function
-// function showMessage(text, buttonText) {
-//   setTimeout(() => {
-//     const messageDiv = document.createElement("div");
-//     messageDiv.setAttribute("id", "message");
-//     messageDiv.textContent = text;
-
-//     // create play again button
-//     const playAgainBtn = document.createElement("button");
-//     playAgainBtn.textContent = buttonText;
-//     playAgainBtn.addEventListener("click", () => {
-//       newGame();
-//       messageDiv.style.opacity = "0";
-//       setTimeout(() => {
-//         messageDiv.remove();
-//       }, 300);
-//     });
-
-//     // add play again button to message div
-//     messageDiv.appendChild(playAgainBtn);
-//     document.body.appendChild(messageDiv);
-
-//     // animate message div
-//     messageDiv.style.opacity = "1";
-//   }, 500);
-// }
-
-// function gameOver() {
-//   lockBoard = true;
-//   showMessage("Bummer! 😵‍💫 You lose!", "Try again");
-// }
-
-// function winGame() {
-//   showMessage("Awesome! 🤘 You Rock!", "Play again");
-//   console.log("Count has reached 12.");
-// }
-
-// function newGame() {
-//   location.reload();
-// }
-
-// // cards.forEach((card) => card.addEventListener("click", flipCard));
+//Restart game
+const restart = (text) => {
+  let cardData = randomize();
+  let faces = document.querySelectorAll(".face");
+  let cards = document.querySelectorAll(".card");
+  section.style.pointerEvents = "none";
+  cardData.forEach((item, index) => {
+    cards[index].classList.remove("toggleCard");
+    //randomize
+    setTimeout(() => {
+      cards[index].style.pointerEvents = "all";
+      faces[index].src = item.imgSrc;
+      cards[index].setAttribute("name", item.name);
+      section.style.pointerEvents = "all";
+    }, 1000);
+  });
+  playerLives = 16;
+  playerLivesCount.textContent = playerLives;
+  // win/lose text
+  // create message div
+  setTimeout(() => {
+    const messageDiv = document.createElement("div");
+    messageDiv.setAttribute("id", "message");
+    messageDiv.textContent = text;
+    // create play again button
+    const playAgainBtn = document.createElement("button");
+    playAgainBtn.textContent = "Play Again";
+    playAgainBtn.addEventListener("click", () => {
+      location.reload();
+      messageDiv.style.opacity = "0";
+      setTimeout(() => {
+        messageDiv.remove();
+      }, 300);
+    });
+    // add play again button to message div
+    messageDiv.appendChild(playAgainBtn);
+    document.body.appendChild(messageDiv);
+    // animate message div
+    messageDiv.style.opacity = "1";
+  }, 500);
+};
 
 cardGenerator();
