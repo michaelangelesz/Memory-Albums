bae64f91be98b305e8b7ab42d76654d5581f44e9
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-23 17:21:09 -0400

    add some JS pseudocode

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Memory-Albums/script.js loaded')
+
+// select memory-game section
+
+// create the card divs
+    //create top div
+    // create bottom div
+
+// append the divs to the memory-game section
